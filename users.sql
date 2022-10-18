69740e89b2f92b998d3eda21aa37d3c040a4cf23
OJ <100131973+Ijohn13@users.noreply.github.com>
2022-10-18 17:45:27 +0200

    delete user.sql

--- users.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `ijohn_tasks_children`.`user` (
-    `iduser` INT NOT NULL AUTO_INCREMENT,
-    `surname` VARCHAR(45) NOT NULL,
-    `password` VARCHAR(500) NOT NULL,
-    PRIMARY KEY (`iduser`));
